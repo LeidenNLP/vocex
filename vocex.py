687b09a305c3f4d7932a9cefb723240a97e13d3e
mwestera <m.westera@hum.leidenuniv.nl>
2024-10-09 23:13:51 +0200

    refactor

--- vocex.py
+++ vocex.py
@@ -1,19 +1,28 @@
 import sys
-import wordcloud    # not actually used; only meant to illustrate how dependencies work
+import wordcloud    # not actually used; only meant to illustrate how dependencies work (see it in the pyproject.toml file?)
 
 """
-Read a text.
-Tokenize it.
-Extract its vocabulary.
-Output the vocabulary.
+A very simplistic, minimalist vocabulary extractor, to illustrate the basics of command-line tools and packaging.
 """
+
 
 def cli():
+    """
+    Command-line interface, just an input-output wrapper around the function extract_vocabulary.
+    """
     text = sys.stdin.read()
+    vocab = extract_vocabulary(text)
+    sys.stdout.write('\n'.join(vocab))
+
+
+def extract_vocabulary(text):
+    """
+    Function doing the heavy-lifting.
+    """
     tokens = text.lower().split()
     vocab = set(tokens)
-    sys.stdout.write('\n'.join(sorted(vocab)))
-
+    vocab_sorted = sorted(vocab)
+    return vocab_sorted
 
 
 if __name__ == '__main__':
